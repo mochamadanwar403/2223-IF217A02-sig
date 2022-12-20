6d3994716067a5af89ff310bed1f872768198759
mochamadanwar403 <65333334+mochamadanwar403@users.noreply.github.com>
2022-12-20 14:04:16 +0700

    Delete tugas_akhir directory

--- tugas_akhir/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = new mysqli ("localhost","root","","perpustakaan");
- if (mysqli_connect_errno()){
-        echo "Koneksi database mysqli gagal!!! : " . mysqli_connect_error();
-    }
-//mysqli_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
-?>
